f142f0cff44236e19334dd2d418e64344abdb703
Torinnnn <torinajanaku@gmail.com>
2022-07-01 23:55:39 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,9 @@
  * main - Prints "with proper grammar, but the output is a piece of art,"
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
