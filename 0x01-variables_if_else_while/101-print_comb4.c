cc036a0edbb3896cc9649ac9ff01dc9920c5074c
Torinnnn <torinajanaku@gmail.com>
2022-07-03 14:55:15 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,17 +11,17 @@ int main(void)
 	int d;
 	int e = 0;
 
-	while (e <= 9)
+	while (e < 10)
 	{
 		d = 0;
 
-		while (d <= 9)
+		while (d < 10)
 		{
 			c = 0;
 
-			while (c <= 9)
+			while (c < 10)
 			{
-				if (c != d && d != e && e < d && d < e)
+				if (c != d && d != e && e < d && d < c)
 				{
 					putchar('0' + e);
 					putchar('0' + d);
