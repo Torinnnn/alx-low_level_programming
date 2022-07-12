e18a86c11735ec1650a50c2d5909c119d32b7af0
Torinnnn <torinajanaku@gmail.com>
2022-07-12 16:43:37 -0700

    correction

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,18 +2,17 @@
 
 /**
  * swap_int - function that swaps two integers
- * @tempc: a integer that stores the values temporarily
  * @a: a first integer whose value is exchanged
  * @b: a second integer whose value is to be exchanged with the first
  *
  * Return: nothing
  */
-void swap_int(int *a, int *b, int tempc)
+void swap_int(int *a, int *b)
 {
-	int tempc;
+	int c
 
-	tempc = *a;
+	 c = *a;
 	*a = *b;
-	*b = tempc;
+	*b = c;
 }
 
