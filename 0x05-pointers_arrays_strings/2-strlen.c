41ac7e014f5a0b95313b43927381743a68977c1e
Torinnnn <torinajanaku@gmail.com>
2022-07-12 21:04:46 -0700

    function to check length of string in c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function to return length of string
+ * @s: the pointer passed as parameter
+ *
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s[i] != '\0){
+		c++;
+	};
+
+	return i;
+}
