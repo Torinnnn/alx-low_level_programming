89ce90abd4b6474d2d63972d6e54fb861c0332c8
Torinnnn <torinajanaku@gmail.com>
2022-07-13 10:45:20 -0700

    function that prints out the element of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - function to print elements of an arra
+ * @a: pointer variable
+ * @n: variable
+ *
+ * Return always 0
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		i++;
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n);
+}
