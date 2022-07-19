a1110d74a09d8473cbf1dae9a828b76d917238ce
Torinnnn <torinajanaku@gmail.com>
2022-07-19 16:04:16 -0700

    correction

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,5 +15,5 @@ void print_diagsums(int *a, int size)
 		sum1 += a[(size + 1) * x];
 	for (y = 0; y < size; y++)
 		sum2 += a[(size - 1) * (y + 1)];
-	print("%d, %d\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
